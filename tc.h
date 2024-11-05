0f4c4385eaf7f69f202cd11d2b3ee94415f0dd3d
Cloud User <nehfaf@ocp-hafifot-2.m413yydec54urplvmchjghawpf.ax.internal.cloudapp.net>
2024-11-05 17:11:33 +0000

    Added basic structs for TC_EVT and Value (BPF_MAP)

---
+++ tc.h
@@ -0,0 +1,47 @@
+#ifndef __TC_H__
+#define __TC_H__
+
+#define TASK_LEN 16
+
+enum ip_proto {
+    TCP_V4,
+    TCP_V6,
+    UDP_V4,
+    UDP_V6,
+};
+
+enum pkt_state {
+    BLOCKED,
+    ALLOWED,
+};
+
+struct ip_info {
+    enum ip_proto ipp;
+     union {
+        uint8_t ipv6_daddr[16];
+        uint8_t ipv4_daddr[4];
+    } daddr;
+
+    union {
+        uint8_t ipv6_saddr[16];
+        uint8_t ipv4_saddr[4];
+    } saddr;
+
+    uint16_t port;
+};
+
+struct tc_evt {
+    enum pkt_state pkt_state;
+    pid_t tgid;
+    pid_t pid;
+    char comm[TASK_LEN];
+    uint16_t eth_type;
+    struct ip_info ip;
+};
+
+struct value {
+    __u64 packets;
+    __u64 bytes;
+};
+
+#endif 
