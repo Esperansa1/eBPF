daea90c0f780eb5a3745d85faaf2b1ca205fff86
Cloud User <nehfaf@ocp-hafifot-2.m413yydec54urplvmchjghawpf.ax.internal.cloudapp.net>
2024-11-10 12:50:36 +0000

    Added block packets by port

--- kernel_space.c
+++ kernel_space.c
@@ -5,11 +5,11 @@
 #include <bpf/bpf_endian.h>
 
 #include "tc.h"
+#include <stdbool.h>
 
 //#include <linux/if_ether.h>
 #define ETH_ALEN 6
 #define ETH_P_IP    0x0800
-#define ETH_P_IPV6  0x86DD
 
 //#include <linux/pkt_cls.h>
 #define TC_ACT_UNSPEC	(-1)
@@ -22,6 +22,8 @@
 #define TC_ACT_REPEAT		6
 #define TC_ACT_REDIRECT		7
 #define TC_ACT_TRAP		8
+
+#define PORT_AMOUNT 20
 
 #define htons bpf_htons
 #define ntohl bpf_ntohl
@@ -37,7 +39,7 @@ struct {
 
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
-    __uint(max_entries, 20);
+    __uint(max_entries, PORT_AMOUNT);
     __type(value, u16);
     __type(key, u32);
 } ports SEC(".maps");
@@ -79,24 +81,6 @@ struct iphdr* is_ipv4(struct ethhdr *eth, void *data_end)
     return iph;
 }
 
-// struct ipv6hdr* is_ipv6(struct ethhdr *eth, void *data_end) 
-// {
-//     struct ipv6hdr *iph = NULL;
-//     if (!eth || !data_end) {
-//         return NULL;
-//     }
-
-//     if ((void*)eth + sizeof(*eth) + sizeof(*iph) > data_end) {
-//         return NULL;
-//     }
-    
-//     if (eth->h_proto == htons(ETH_P_IPV6)) {
-//         iph = (struct ipv6hdr*)((void*)eth + sizeof(*eth));
-//     }
-//     return iph;
-// }
-
-
 struct udphdr* is_udp(void *iph, u8 hdr_sz, void *data_end)
 {
     struct udphdr *udph = NULL;
@@ -112,9 +96,6 @@ struct udphdr* is_udp(void *iph, u8 hdr_sz, void *data_end)
     if (hdr_sz == sizeof(struct iphdr)) {
         struct iphdr *v4 = (struct iphdr*)iph;
         proto = v4->protocol;
-    } else if (hdr_sz == sizeof(struct ipv6hdr)) {
-        struct ipv6hdr *v6 = (struct ipv6hdr*)iph;
-        proto = v6->nexthdr;
     }
 
     if (proto == IPPROTO_UDP) {
@@ -138,15 +119,27 @@ struct tcphdr* is_tcp(void *iph, u8 hdr_sz, void *data_end)
     if (hdr_sz == sizeof(struct iphdr)) {
         struct iphdr *v4 = (struct iphdr*)iph;
         proto = v4->protocol;
-    } else if (hdr_sz == sizeof(struct ipv6hdr)) {
-        struct ipv6hdr *v6 = (struct ipv6hdr*)iph;
-        proto = v6->nexthdr;
     }
 
     if (proto == IPPROTO_TCP) {
         tcph = (struct tcphdr*)((void*)iph + hdr_sz);
     }
     return tcph;
+}
+
+int is_port_allowed(int target_port){
+    u32 i = 0;
+    for (i = 0; i < PORT_AMOUNT; i++) {
+        u16 *port;
+        u32 key = i;
+        port = bpf_map_lookup_elem(&ports, &key);
+
+        // check if the packet's port is within the allowed ports or the user allowed all ports (default port -1)
+        if (port && (target_port == *port || *port == (u16)ALL_PORTS_ALLOWED)) {  // comparison of u16 and int is always false so cast to u16
+            return true;
+        }
+    }
+    return false;
 }
 
 
@@ -162,7 +155,6 @@ int handle_egress(struct __sk_buff *skb)
     void *data_end = (void*)(long)skb->data_end;
     struct ethhdr *eth = (struct ethhdr*)(void*)(long)skb->data;
     struct iphdr *iph = is_ipv4(eth, data_end);
-    // struct ipv6hdr *iph6 = is_ipv6(eth, data_end);
 
     struct tc_evt *evt = NULL;
     evt = bpf_ringbuf_reserve(&rb, sizeof(*evt), 0);
@@ -174,45 +166,39 @@ int handle_egress(struct __sk_buff *skb)
     bpf_probe_read_kernel_str(evt->comm, TASK_LEN, BPF_CORE_READ(t, group_leader, comm));
     evt->tgid = tgid;
     evt->pid = pid;
+    evt->pkt_state = BLOCKED; // Block packet by default (for showing in C userspace)
 
     int rc = TC_ACT_SHOT; // Drop the packet by default
-
-    u32 i = 0;
-    for (i = 0; i < 10; i++) {
-        u16 *port;
-        u32 key = i;
-        port = bpf_map_lookup_elem(&ports, &key);
-        if (port && evt->ip.port == *port) {
-            rc = TC_ACT_OK;
-        }
-    }
 
     if (iph) {
         u8 hdr_sz = sizeof(*iph);
         struct udphdr *udph = is_udp(iph, hdr_sz, data_end);
         struct tcphdr *tcph = is_tcp(iph, hdr_sz, data_end);
 
+        if (tcph) {
+            evt->ip.ipp = TCP_V4;
+            evt->ip.port = ntohs(tcph->dest);
+        } else if(udph) {
+            evt->ip.ipp = UDP_V4;
+            evt->ip.port = ntohs(udph->dest);
+        }else{
+            goto err;
+        }
+
         u32 daddr = iph->daddr;
         u32 saddr = iph->saddr;
 
-        if (tcph || udph) {
+        if(is_port_allowed(evt->ip.port)){
+
+            evt->pkt_state = ALLOWED;
+            rc = TC_ACT_OK;
+
             bpf_probe_read_kernel(&evt->ip.daddr.ipv4_daddr, sizeof(evt->ip.daddr.ipv4_daddr), &daddr);
             bpf_probe_read_kernel(&evt->ip.saddr.ipv4_saddr, sizeof(evt->ip.saddr.ipv4_saddr), &saddr);
-            
-            // Add stats to both sender and reciever on the condition that the packet is within the allowed ports
-            if(rc == TC_ACT_OK){
-                update_stats(saddr, iph->tot_len);
-                update_stats(daddr, iph->tot_len);
-            }
-        } else {
-            goto err; 
-        }
-    }
 
-    if (rc == TC_ACT_SHOT) {
-        evt->pkt_state = BLOCKED;
-    } else {
-        evt->pkt_state = ALLOWED;
+            // Add stats to the destination on the condition that the packet is within the allowed ports
+            update_stats(daddr, iph->tot_len);
+        }
     }
 
     bpf_ringbuf_submit(evt, 0);
@@ -220,7 +206,7 @@ int handle_egress(struct __sk_buff *skb)
     err:
         if (evt) bpf_ringbuf_discard(evt, 0);
     rb_err:
-        return TC_ACT_OK; // Need to return rc here but do not want to block any packets to not block myself out
-    }
+        return TC_ACT_OK; // Need to return rc value here but do not want to block any packets to not block myself out of SSH
+}
 
 char LICENSE[] SEC("license") = "GPL";
