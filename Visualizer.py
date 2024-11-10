e8e457d45c9ba8fae19bb1fddac994bd5ab7ddaf
Cloud User <nehfaf@ocp-hafifot-2.m413yydec54urplvmchjghawpf.ax.internal.cloudapp.net>
2024-11-10 12:49:43 +0000

    Fixed visualize ip load to work with given paramaters

--- Visualizer.py
+++ Visualizer.py
@@ -1,6 +1,4 @@
 from itertools import count
-import math
-import numpy
 import os
 
 import matplotlib.pyplot as plt
@@ -19,7 +17,6 @@ class Visualizer:
 
         # Graph Animation
         self.fig, self.ax = plt.subplots()
-        self.ax.legend(title='Packets per second')
 
     def reset_visualization(self):
         self.index = count(step=self.delay)
@@ -35,13 +32,9 @@ class Visualizer:
         plt.show()
         plt.cla()
     
-    def visualize_ip_load(self, data, ip):
+    def visualize_ip_load(self, load):
         self.x_values.append(next(self.index))
-        packet_load = 0
-        for packet in data:
-            if packet['addr'] == ip:
-                packet_load = packet['packets']
-        self.y_values.append(packet_load)
+        self.y_values.append(load)
         
         os.system("clear")
         plt.plot(self.x_values, self.y_values)
